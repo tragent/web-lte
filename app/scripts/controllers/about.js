bc6ef4a5bfe77f00a8657a434139a09b5f947617
Anh3h <courageangeh@gmail.com>
2017-05-31 11:09:32 +0100

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webLteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webLteApp
+ */
+angular.module('webLteApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
