14bd2664a84905a1e05b79ec3553b2e604481a2b
kengneruphine <ruphinekengne@gmail.com>
2017-06-01 10:16:05 +0100

    Implement skeleton code

---
+++ app/scripts/controllers/dashboard.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webLteApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webLteApp
+ */
+angular.module('webLteApp')
+  .controller('DashboardCtrl', function($scope) {   
+  });
+  
+ $(".button-collapse").sideNav();
